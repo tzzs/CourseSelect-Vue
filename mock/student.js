0b19049f9ceaf84e365bcbc2005127f3bc42579e
tzzs <1789803837@qq.com>
2019-05-29 16:00:24 +0800

    finish student search modules

--- mock/student.js
+++ mock/student.js
@@ -1,20 +1,25 @@
 // import Mock from 'mockjs'
 const Mock = require('mockjs')
 
-const List = Mock.mock({
-  'id': '@id',
-  'stuid': '@id',
-  'name': '@first',
-  'password': '@integer(10,20)',
-  'email': '@email',
-  'sex|1': ['男', '女'],
-  'college': '@ctitle',
-  'profession': '@ctitle',
-  'class': '@natural',
-  'phone': '@natural',
-  'regTime': '@date(yyyy-MM-dd)',
-  'lastTime': '@date(yyyy-MM-dd)'
-})
+const List = []
+const count = 100
+
+for (let i = 0; i < count; i++) {
+  List.push(Mock.mock({
+    'id': '@id',
+    'stuid': '@id',
+    'name': '@first',
+    'password': '@integer(10,20)',
+    'email': '@email',
+    'sex|1': ['男', '女'],
+    'college': '@ctitle',
+    'profession': '@ctitle',
+    'class': '@natural',
+    'phone': '@natural',
+    'regTime': '@date(yyyy-MM-dd)',
+    'lastTime': '@date(yyyy-MM-dd)'
+  }))
+}
 
 export default [{
   url: '/student/profile',
@@ -42,11 +47,33 @@ export default [{
   url: '/student/list',
   type: 'get',
   response: config => {
-    return {
-      code: 20000,
-      data: {
-        total: List.length,
-        items: List
+    const { page = 1, limit = 10, search } = config.query
+    // console.log(config.query)
+    if (search) {
+      var mockList = List.filter(item => {
+        if (item.name.indexOf(search) >= 0 || item.stuid.toString().indexOf(search) >= 0 || item.college.indexOf(search) >= 0 || item.class.toString().indexOf(search) >= 0 || item.profession.indexOf(search) >= 0) {
+          return true
+        }
+      })
+      const pageList = mockList.filter((item, index) => index < limit * page && index >= limit * (page - 1))
+
+      return {
+        code: 20000,
+        data: {
+          total: mockList.length,
+          items: pageList
+        }
+      }
+    } else {
+      mockList = List
+      const pageList = mockList.filter((item, index) => index < limit * page && index >= limit * (page - 1))
+
+      return {
+        code: 20000,
+        data: {
+          total: List.length,
+          items: pageList
+        }
       }
     }
   }
--- src/views/manage/students/index.vue
+++ src/views/manage/students/index.vue
@@ -1,60 +1,101 @@
 <template>
   <div class="app-container">
-    <el-checkbox-group v-model="checkList">
-      <el-table
-        v-loading="listLoading"
-        :data="list"
-        border
-        fit
-        highlight-current-row
-        style="width: 100%"
+    <div class="filter-container">
+      <el-input
+        v-model="listQuery.search"
+        placeholder="搜索内容"
+        style="width: 350px;"
+        class="filter-item"
+        @keyup.enter.native="handleFilter"
+      />
+      <el-button v-waves class="filter-item" type="primary" icon="el-icon-search" @click="handleFilter">
+        Search
+      </el-button>
+      <el-button
+        class="filter-item"
+        style="margin-left: 10px;"
+        type="primary"
+        icon="el-icon-edit"
+        @click="handleCreate"
       >
-        <el-table-column align="center" label="ID">
-          <template slot-scope="scope">
-            <el-checkbox :label="scope.row.id" />
-          </template>
-        </el-table-column>
+        Add
+      </el-button>
+      <el-button
+        v-waves
+        :loading="downloadLoading"
+        class="filter-item"
+        type="primary"
+        icon="el-icon-download"
+        @click="handleDownload"
+      >
+        Export
+      </el-button>
+      <el-checkbox v-model="showHidden" class="filter-item" style="margin-left:15px;" @change="tableKey=tableKey+1">
+        Show Hidden Column
+      </el-checkbox>
+    </div>
 
-        <el-table-column width="180px" align="center" label="课程">
-          <template slot-scope="scope">
-            <span>{{ scope.row.name }}</span>
-          </template>
-        </el-table-column>
-
-        <el-table-column width="120px" align="center" label="教师">
-          <template slot-scope="scope">
-            <span>{{ scope.row.teacher }}</span>
-          </template>
-        </el-table-column>
-
-        <el-table-column width="100px" align="center" label="学时">
-          <template slot-scope="scope">
-            <span>{{ scope.row.period }}</span>
-          </template>
-        </el-table-column>
-
-        <el-table-column class-name="status-col" align="center" label="剩余可选" width="110">
-          <template slot-scope="scope">
-            <span>{{ scope.row.stu_number }}</span>
-          </template>
-        </el-table-column>
-
-        <el-table-column min-width="300px" align="center" label="上课时间">
-          <template slot-scope="scope">
-            <span>{{ scope.row.time }}</span>
-          </template>
-        </el-table-column>
-
-        <el-table-column align="center" label="Actions" width="120">
-          <template slot-scope="scope">
-            <router-link :to="'/example/edit/'+scope.row.id">
-              <el-button type="primary" size="small" icon="el-icon-edit">Edit</el-button>
-            </router-link>
-          </template>
-          <!-- <el-checkbox label></el-checkbox> -->
-        </el-table-column>
-      </el-table>
-    </el-checkbox-group>
+    <el-table
+      :key="tableKey"
+      v-loading="listLoading"
+      :data="list"
+      border
+      fit
+      highlight-current-row
+      style="width: 100%;"
+      @sort-change="sortChange"
+    >
+      <el-table-column label="学号" prop="id" min-width="150px" sortable>
+        <template slot-scope="scope">
+          <span>{{ scope.row.stuid }}</span>
+        </template>
+      </el-table-column>
+      <el-table-column label="姓名" align="center">
+        <template slot-scope="scope">
+          <span>{{ scope.row.name }}</span>
+        </template>
+      </el-table-column>
+      <el-table-column v-if="showHidden" label="性别" align="center">
+        <template slot-scope="scope">
+          <span style="color:red;">{{ scope.row.sex }}</span>
+        </template>
+      </el-table-column>
+      <el-table-column label="邮箱" align="center">
+        <template slot-scope="scope">
+          <span>{{ scope.row.email }}</span>
+        </template>
+      </el-table-column>
+      <el-table-column label="电话">
+        <template slot-scope="scope">
+          <span>{{ scope.row.phone }}</span>
+        </template>
+      </el-table-column>
+      <el-table-column label="学院" align="center">
+        <template slot-scope="{row}">
+          <span>{{ row.college }}</span>
+        </template>
+      </el-table-column>
+      <el-table-column label="班级" align="center">
+        <template slot-scope="scope">
+          <span>{{ scope.row.class }}</span>
+        </template>
+      </el-table-column>
+      <el-table-column label="专业" align="center">
+        <template slot-scope="scope">
+          <span>{{ scope.row.profession }}</span>
+        </template>
+      </el-table-column>
+      <el-table-column label="Actions" align="center" class-name="small-padding fixed-width" min-width="160">
+        <template slot-scope="{row}">
+          <el-button type="primary" size="mini" @click="handleUpdate(row)">
+            Edit
+          </el-button>
+          <el-button size="mini" type="danger" @click="handleDelete(row)">
+            Delete
+          </el-button>
+        </template>
+      </el-table-column>
+    </el-table>
 
     <pagination
       v-show="total>0"
@@ -64,86 +105,266 @@
       @pagination="getList"
     />
 
-    <div style="border-radius: 4px;width: 100%;box-shadow: 0 2px 4px rgba(0, 0, 0, .12);padding: 0 0 24px 32px;">
-      <span>当前选择科目：{{ count }} 总学分：{{ count }}</span>
-      <el-button type="primary" style="margin: 0 25px" @click="open()">提交</el-button>
-      <span>{{ checkList }}</span>
-    </div>
+    <el-dialog :title="textMap[dialogStatus]" :visible.sync="dialogFormVisible">
+      <el-form
+        ref="dataForm"
+        :rules="rules"
+        :model="temp"
+        label-position="left"
+        label-width="70px"
+        style="width: 400px; margin-left:50px;"
+      >
+        <el-form-item v-show="false" label="id" prop="id">
+          <el-input v-model="temp.id" />
+        </el-form-item>
+        <el-form-item label="教工号" prop="teaid">
+          <el-input v-model="temp.teaid" />
+        </el-form-item>
+        <el-form-item label="姓名" prop="name">
+          <el-input v-model="temp.name" />
+        </el-form-item>
+        <el-form-item label="职称" prop="title">
+          <el-input v-model="temp.title" />
+        </el-form-item>
+        <el-form-item label="邮箱" prop="email">
+          <el-input v-model="temp.email" />
+        </el-form-item>
+        <el-form-item label="手机" prop="phone">
+          <el-input v-model="temp.phone" />
+        </el-form-item>
+        <el-form-item label="学院" prop="college">
+          <el-input v-model="temp.college" />
+        </el-form-item>
+      </el-form>
+      <div slot="footer" class="dialog-footer">
+        <el-button @click="dialogFormVisible = false">
+          Cancel
+        </el-button>
+        <el-button type="primary" @click="dialogStatus==='create'?createData():updateData()">
+          Confirm
+        </el-button>
+      </div>
+    </el-dialog>
+
+    <el-dialog :visible.sync="dialogPvVisible" title="Reading statistics">
+      <el-table :data="pvData" border fit highlight-current-row style="width: 100%">
+        <el-table-column prop="key" label="Channel" />
+        <el-table-column prop="pv" label="Pv" />
+      </el-table>
+      <span slot="footer" class="dialog-footer">
+        <el-button type="primary" @click="dialogPvVisible = false">Confirm</el-button>
+      </span>
+    </el-dialog>
   </div>
 </template>
 
 <script>
-import { fetchList } from '@/api/course'
-import Pagination from '@/components/Pagination' // Secondary package based on el-pagination
+import { fetchList, createStudent, updateStudent } from '@/api/student'
+import waves from '@/directive/waves' // waves directive
+import { parseTime } from '@/utils'
+import Pagination from '@/components/Pagination' // secondary package based on el-pagination
 
 export default {
-  name: 'CourseList',
+  name: 'ComplexTable',
   components: { Pagination },
+  directives: { waves },
   filters: {
     statusFilter(status) {
       const statusMap = {
-        published: 'success',
-        draft: 'info',
-        deleted: 'danger'
+        Optional: 'success',
+        NotOptional: 'danger'
       }
       return statusMap[status]
     }
   },
   data() {
     return {
-      checkList: [],
+      tableKey: 0,
       list: null,
       total: 0,
       listLoading: true,
       listQuery: {
         page: 1,
-        limit: 20
+        limit: 10,
+        sort: '+id',
+        search: undefined
       },
-      count: 5
+      sortOptions: [{ label: 'ID Ascending', key: '+id' }, { label: 'ID Descending', key: '-id' }],
+      statusOptions: ['Optional', 'NotOptional'],
+      showHidden: false,
+      showSemester: false,
+      temp: {
+        name: '',
+        stuid: '',
+        email: '',
+        phone: '',
+        college: '',
+        title: ''
+      },
+      dialogFormVisible: false,
+      dialogStatus: '',
+      textMap: {
+        update: 'Edit',
+        create: 'Create'
+      },
+      dialogPvVisible: false,
+      pvData: [],
+      rules: {
+        teaid: [{ required: true, message: 'teacher id is required', trigger: 'blur' }]
+      },
+      downloadLoading: false
     }
   },
   created() {
     this.getList()
   },
   methods: {
-    // 获取课程信息
     getList() {
       this.listLoading = true
       fetchList(this.listQuery).then(response => {
+        console.log(response.data)
         this.list = response.data.items
         this.total = response.data.total
-        this.listLoading = false
+        // console.log(this.list)
+
+        // Just to simulate the time of the request
+        setTimeout(() => {
+          this.listLoading = false
+        }, 1.5 * 1000)
       })
     },
-    open() {
-      this.$confirm('当前选择科目：' + this.count + '  总学分：' + this.count, '提交', {
-        confirmButtonText: '确定',
-        cancelButtonText: '取消',
-        type: 'warning'
-      }).then(() => {
-        this.$message({
-          type: 'successs',
-          message: '提交成功'
-        })
-      }).catch(() => {
-        this.$message({
-          type: 'info',
-          message: '已取消'
-        })
+    handleFilter() {
+      this.listQuery.page = 1
+      this.getList()
+    },
+    handleModifyStatus(row, status) {
+      this.$message({
+        message: '操作Success',
+        type: 'success'
       })
+      row.status = status
+    },
+    sortChange(data) {
+      const { prop, order } = data
+      if (prop === 'id') {
+        this.sortByID(order)
+      }
+    },
+    sortByID(order) {
+      if (order === 'ascending') {
+        this.listQuery.sort = '+id'
+      } else {
+        this.listQuery.sort = '-id'
+      }
+      this.handleFilter()
+    },
+    resetTemp() {
+      this.temp = {
+        name: '',
+        credit: '',
+        teacher: '',
+        stu_number: '',
+        semester: '',
+        status: '',
+        rate: 3
+      }
+    },
+    handleCreate() {
+      this.resetTemp()
+      this.dialogStatus = 'create'
+      this.dialogFormVisible = true
+      this.$nextTick(() => {
+        this.$refs['dataForm'].clearValidate()
+      })
+    },
+    createData() {
+      this.$refs['dataForm'].validate((valid) => {
+        if (valid) {
+          createTeaher(this.temp).then(() => {
+            this.list.unshift(this.temp)
+            this.dialogFormVisible = false
+            this.$notify({
+              title: 'Success',
+              message: 'Created Successfully',
+              type: 'success',
+              duration: 2000
+            })
+          })
+        }
+      })
+    },
+    handleUpdate(row) {
+      this.temp = Object.assign({}, row) // copy obj
+      this.temp.timestamp = new Date(this.temp.timestamp)
+      this.dialogStatus = 'update'
+      this.dialogFormVisible = true
+      this.$nextTick(() => {
+        this.$refs['dataForm'].clearValidate()
+      })
+    },
+    updateData() {
+      this.$refs['dataForm'].validate((valid) => {
+        if (valid) {
+          const tempData = Object.assign({}, this.temp)
+          tempData.timestamp = +new Date(tempData.timestamp) // change Thu Nov 30 2017 16:41:05 GMT+0800 (CST) to 1512031311464
+          updateTeacher(tempData).then(() => {
+            for (const v of this.list) {
+              if (v.id === this.temp.id) {
+                const index = this.list.indexOf(v)
+                this.list.splice(index, 1, this.temp)
+                break
+              }
+            }
+            this.dialogFormVisible = false
+            this.$notify({
+              title: 'Success',
+              message: 'Update Successfully',
+              type: 'success',
+              duration: 2000
+            })
+          })
+        }
+      })
+    },
+    handleDelete(row) {
+      this.$notify({
+        title: 'Success',
+        message: 'Delete Successfully',
+        type: 'success',
+        duration: 2000
+      })
+      const index = this.list.indexOf(row)
+      this.list.splice(index, 1)
+    },
+    handleFetchPv(pv) {
+      fetchPv(pv).then(response => {
+        this.pvData = response.data.pvData
+        this.dialogPvVisible = true
+      })
+    },
+    handleDownload() {
+      this.downloadLoading = true
+      import('@/vendor/Export2Excel').then(excel => {
+        const tHeader = ['timestamp', 'title', 'type', 'importance', 'status']
+        const filterVal = ['timestamp', 'title', 'type', 'importance', 'status']
+        const data = this.formatJson(filterVal, this.list)
+        excel.export_json_to_excel({
+          header: tHeader,
+          data,
+          filename: 'table-list'
+        })
+        this.downloadLoading = false
+      })
+    },
+    formatJson(filterVal, jsonData) {
+      return jsonData.map(v => filterVal.map(j => {
+        if (j === 'timestamp') {
+          return parseTime(v[j])
+        } else {
+          return v[j]
+        }
+      }))
     }
   }
 }
 </script>
-
-<style scoped>
-  .edit-input {
-    padding-right: 100px;
-  }
-
-  .cancel-btn {
-    position: absolute;
-    right: 15px;
-    top: 10px;
-  }
-</style>
