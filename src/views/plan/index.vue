445d4f9e79fd893eb2f241c0c7ac05feca15e29e
tzzs <1789803837@qq.com>
2019-06-01 20:56:14 +0800

    修改布局方式 增加培养计划

--- src/views/plan/index.vue
+++ src/views/plan/index.vue
@@ -1,14 +1,50 @@
 <template>
   <div class="app-container">
     <div class="filter-container">
-      123
+      <el-tabs type="border-card">
+        <el-tab-pane>
+          <span slot="label"><svg-icon icon-class="plan" /> &nbsp; 全部计划</span>
+          <plan1 />
+        </el-tab-pane>
+        <el-tab-pane>
+          <span slot="label"><svg-icon icon-class="add" /> &nbsp; 添加计划</span>
+          <el-steps :active="active" finish-status="success">
+            <el-step title="步骤 1">
+              123123123
+            </el-step>
+            <el-step title="步骤 2" />
+            <el-step title="步骤 3" />
+            <el-step title="步骤 4" />
+            <el-step title="步骤 5" />
+            <el-step title="步骤 6" />
+            <el-step title="步骤 7" />
+            <el-step title="步骤 8" />
+          </el-steps>
+
+          <el-button style="margin-top: 12px;" @click="next">下一步</el-button>
+        </el-tab-pane>
+      </el-tabs>
     </div>
   </div>
 </template>
 
 <script>
+import plan1 from './plan1'
 export default {
-  name: 'Plan'
+  name: 'Plan',
+  components: {
+    plan1
+  },
+  data() {
+    return {
+      active: 1
+    }
+  },
+  methods: {
+    next() {
+      if (this.active++ > 7) this.active = 1
+    }
+  }
 }
 </script>
 
