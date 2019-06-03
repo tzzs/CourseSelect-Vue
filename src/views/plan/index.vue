171a0282f25679e0274f9288cedbad923d716976
tzzs <1789803837@qq.com>
2019-06-03 15:14:07 +0800

    培养计划修改

--- src/views/plan/index.vue
+++ src/views/plan/index.vue
@@ -1,15 +1,16 @@
 <template>
   <div class="app-container">
     <div class="filter-container">
-      <el-tabs type="border-card">
-        <el-tab-pane>
-          <span slot="label"><svg-icon icon-class="plan" /> &nbsp; 全部计划</span>
-          <allplan />
-        </el-tab-pane>
-        <el-tab-pane>
-          <span slot="label"><svg-icon icon-class="add" /> &nbsp; 计划管理</span>
-        </el-tab-pane>
-      </el-tabs>
+      <allplan />
+      <!--      <el-tabs type="border-card">-->
+      <!--        <el-tab-pane>-->
+      <!--          <span slot="label"><svg-icon icon-class="plan" /> &nbsp; 全部计划</span>-->
+      <!--          <allplan />-->
+      <!--        </el-tab-pane>-->
+      <!--        <el-tab-pane>-->
+      <!--          <span slot="label"><svg-icon icon-class="add" /> &nbsp; 计划管理</span>-->
+      <!--        </el-tab-pane>-->
+      <!--      </el-tabs>-->
     </div>
   </div>
 </template>
